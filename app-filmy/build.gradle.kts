703101a5f87aac6a88ee02e4f1cde66994dc96ae
Jan Koran <jankoran90@gmail.com>
2026-08-01 18:59:34 +0200

    Filmy 1.2.23 / vc134

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 133
-        versionName = "1.2.22"
+        versionCode = 134
+        versionName = "1.2.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
