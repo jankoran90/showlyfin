a74695be7ee24d3782fe33eb9c20039100b1b5d2
Jan Koran <jankoran90@gmail.com>
2026-08-13 17:03:24 +0200

    DROPSHIP F4: bump Slovo vc3/1.0.2 (release Nahrát audioknihu)

--- app-slovo/build.gradle.kts
+++ app-slovo/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.jankoran90.slovo"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
