91d8bb7ac43e07a7ad2e9814e82ce8cb914ce9d7
Jan Koran <jankoran90@gmail.com>
2026-08-21 20:58:08 +0200

    Filmy: pridat data-offline zavislost do ui-filmy-phone + chybejici dp import

--- ui-filmy-phone/src/main/kotlin/com/github/jankoran90/showlyfin/ui/filmyphone/FilmyDownloadsScreen.kt
+++ ui-filmy-phone/src/main/kotlin/com/github/jankoran90/showlyfin/ui/filmyphone/FilmyDownloadsScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.jankoran90.showlyfin.core.domain.MediaItem
