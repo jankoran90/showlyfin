84facbfe1691e110ef569ffce150d0efa62514d6
Jan Koran <jankoran90@gmail.com>
2026-08-22 20:30:20 +0200

    Slovo: bump 1.0.57->1.0.58/vc57->vc58

--- app-slovo/build.gradle.kts
+++ app-slovo/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.jankoran90.slovo"
         minSdk = 23
         targetSdk = 36
-        versionCode = 57
-        versionName = "1.0.57"
+        versionCode = 58
+        versionName = "1.0.58"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
