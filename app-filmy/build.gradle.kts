2f68560db25c89317995c3a34a5c484d4713b8dd
Jan Koran <jankoran90@gmail.com>
2026-08-26 18:33:06 +0200

    Filmy: bump 1.2.86/vc197 (rada Dalsi dily z disku i bez cache baze)

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 196
-        versionName = "1.2.85"
+        versionCode = 197
+        versionName = "1.2.86"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
