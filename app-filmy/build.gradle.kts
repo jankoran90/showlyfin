45d774131d89ecaaa0c9219a3ed514c6aeffee3f
Jan Koran <jankoran90@gmail.com>
2026-08-26 17:10:05 +0200

    Filmy: bump 1.2.82/vc193 (atomicka publikace profil+config)

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 192
-        versionName = "1.2.81"
+        versionCode = 193
+        versionName = "1.2.82"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
