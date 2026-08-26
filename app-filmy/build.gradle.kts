42c0fdb6e0dd6d3e0b0f40ab96c461acdbcebf3d
Jan Koran <jankoran90@gmail.com>
2026-08-26 17:51:23 +0200

    Filmy: bump 1.2.83/vc194 (zdroje ciziho profilu se po prepnuti nesmi servirovat ani cachovat)

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 193
-        versionName = "1.2.82"
+        versionCode = 194
+        versionName = "1.2.83"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
