5e570491f890b6b7d5cb7d81f589ef46b1e653b2
Jan Koran <jankoran90@gmail.com>
2026-08-26 18:58:06 +0200

    Filmy: bump 1.2.87/vc198 (Odebrat z Filmoteky blokuje i budouci zapisy titulu)

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 197
-        versionName = "1.2.86"
+        versionCode = 198
+        versionName = "1.2.87"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
