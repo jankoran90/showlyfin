363721644084742d0d15124aa69ab28e6a94c52a
Jan Koran <jankoran90@gmail.com>
2026-08-28 21:33:34 +0200

    Filmy: MUZA tlačítko + úplný text + sdílecí karta 1.2.102/vc213

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 212
-        versionName = "1.2.101"
+        versionCode = 213
+        versionName = "1.2.102"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
