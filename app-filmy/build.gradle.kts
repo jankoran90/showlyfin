f4cecc455d6a1dcfd4952fa20b62588e00e55b87
Jan Koran <jankoran90@gmail.com>
2026-08-28 21:13:28 +0200

    Filmy: MUZA 1.2.101/vc212

--- app-filmy/build.gradle.kts
+++ app-filmy/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.jankoran90.filmy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 211
-        versionName = "1.2.100"
+        versionCode = 212
+        versionName = "1.2.101"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
